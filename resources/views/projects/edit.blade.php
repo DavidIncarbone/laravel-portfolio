27c870d36944f7540a6e6641fb0b8c446de29ddf
DavidIncarbone <david.incarbone@gmail.com>
2025-04-02 14:57:25 +0200

    add edit route

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,18 @@
+@extends("layouts.master");
+
+@section("content")
+
+<x-form>
+    
+<x-slot:route>{{route("projects.update", $project )}}</x-slot>
+<x-slot:method>@method("PUT")</x-slot>
+<x-slot:name>{{$project->name}}</x-slot>
+<x-slot:customer>{{$project->customer}}</x-slot>
+<x-slot:period>{{$project->period}}</x-slot>
+<x-slot:summary>{{$project->summary}}</x-slot>
+<x-slot:link><{{$project->link}}</x-slot>
+
+</x-form>
+
+@endsection
+
