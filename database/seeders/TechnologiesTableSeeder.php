b7fb20ae7885d875712009bd29b9b510e3d183db
DavidIncarbone <david.incarbone@gmail.com>
2025-04-04 11:58:03 +0200

    Create pivot table with foreign keys and add methos on models for many to many relation

--- database/seeders/TechnologiesTableSeeder.php
+++ database/seeders/TechnologiesTableSeeder.php
@@ -4,12 +4,26 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
+
+use Faker\Generator as Faker;
 
 class TechnologiesTableSeeder extends Seeder
 {
 
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+
+        $technologies = ["HTML", "CSS", "JavaScript", "React.js", "Node.js", "Express.js", "MySQL"];
+
+        foreach ($technologies as $technology) {
+
+            $newTechnology = new Technology;
+
+            $newTechnology->name = $technology;
+            $newTechnology->color = $faker->hexColor();
+
+            $newTechnology->save();
+        }
     }
 }
